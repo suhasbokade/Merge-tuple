2d2a3d554793a31767109904c4de13528537fe14
Suhas Bokade <99481658+suhasbokade@users.noreply.github.com>
2022-02-11 18:17:16 +0530

    Add files via upload

---
+++ merge tuple.py
@@ -0,0 +1,10 @@
+T = ("2", "1", "4", "6", "2", "1", "4", "1", "2", "7", "1")
+#Write the code to count the frequency of 1.
+
+print(T.count("1"))
+
+#Write a code to merge tuple t with (11, 12, 13, 14) and store in variable named newt.
+
+S = ("11", "12", "13", "14")
+newt = (T + S)
+print(newt)
